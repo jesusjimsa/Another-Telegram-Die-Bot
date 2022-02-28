c707bef45b39a0cdf3bd0c9e779536f55bf1de58
jesusjimsa <jesusjimsa@icloud.com>
2022-02-28 11:26:21 +0000

    Respond with stickers and text
    It is done using the URL because I haven't been able to find the correct method in the library.

--- atd_bot.py
+++ atd_bot.py
@@ -5,15 +5,39 @@ Created by Jesús Jiménez Sánchez.
 """
 
 from random import randint
+import requests
 from telegram.ext import Updater, CommandHandler
 from tg_token import API_TOKEN
+from sticker_ids import ALL
+
+
+URL = "https://api.telegram.org/bot{}/".format(API_TOKEN)
+
+
+def get_url(url):
+    '''
+        Send request to the Telegram API.
+    '''
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def send_sticker(chat_id, die_result):
+    '''
+        Respond with sticker.
+    '''
+    url = URL + "sendSticker?sticker={}&chat_id={}".format(die_result, chat_id)
+    get_url(url)
 
 
 def roll(update, _context):
     '''
         Roll the dice.
     '''
-    update.message.reply_text(randint(1, 6))
+    die_result = randint(1, 6)
+    send_sticker(update.message.chat_id, ALL[die_result - 1])
+    update.message.reply_text(die_result)
 
 
 def main():
